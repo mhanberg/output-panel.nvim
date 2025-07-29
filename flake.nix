a89e0736755e274fd22776ea28c9f02f03095ffb
Philipp Müller <hello@philipp.software>
2025-07-29 17:26:16 +0200

    fix: remove windows from systems configuration in flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" "aarch64-windows"];
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
       perSystem = {
         pkgs,
