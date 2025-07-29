20938a5537761011b989958e3835585deddf63b4
Philipp <philipp.mue@protonmail.com>
2025-07-29 17:40:35 +0200

    chore: add package output in nix flake file (#10)

--- flake.nix
+++ flake.nix
@@ -1,15 +1,31 @@
 {
-  description = "Output Panel";
+  description = "A panel to view the logs from your LSP servers.";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages.default = pkgs.vimUtils.buildVimPlugin {
+          pname = "output-panel-nvim";
+          version = "1.0.0"; # x-release-please-version
+          src = self;
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             bash
