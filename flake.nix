c05129453d47eb0eff28c46c904897630c614c2e
Philipp Müller <hello@philipp.software>
2025-07-29 13:56:42 +0200

    feat: package the plugin using buildVimPlugin

--- flake.nix
+++ flake.nix
@@ -1,22 +1,38 @@
 {
-  description = "Output Panel";
+  description = "A panel to view the logs from your LSP servers.";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bash
-            luarocks
-            lua5_1
-            just
-            stylua
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" "aarch64-windows"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages.default = pkgs.vimUtils.buildVimPlugin {
+          pname = "output-panel-nvim";
+          version = "unstable";
+          src = self;
+          # dependencies = with pkgs.vimPlugins; [
+          #   nui-nvim
+          #   nvim-dap
+          #   nvim-nio
+          #   nvim-dap-virtual-text
+          #   noice-nvim
+          # ];
+          nvimSkipModule = [
           ];
         };
       };
